9a4ee5f954069d43d49db296b58a960eef54c68d
adil <adilsaid64@gmail.com>
2026-01-05 18:56:21 +0000

    refactor: constants into vars and added pgadmin to view data

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import click
 
+RAW_CSV_PATH = "data/taxi_tripdata.csv"
+RAW_TABLE_NAME = "taxi_trips"
+POSTGRES_URI = "postgresql+psycopg2://admin:password@localhost:5432/mydb"
+
+FEATURES_TABLE = "taxi_trip_features"
+
 
 @click.command()
 @click.option(
@@ -13,9 +19,9 @@ def main(pipeline: str) -> None:
         from ingestion.load_csv_to_postgres import CsvToPostgresLoader
 
         loader = CsvToPostgresLoader(
-            csv_path="data/taxi_tripdata.csv",
-            table_name="green_taxi_trips",
-            postgres_uri="postgresql+psycopg2://admin:password@localhost:5432/mydb",
+            csv_path=RAW_CSV_PATH,
+            table_name=RAW_TABLE_NAME,
+            postgres_uri=POSTGRES_URI,
         )
         loader.run()
     elif pipeline == "aggregate":
@@ -24,9 +30,9 @@ def main(pipeline: str) -> None:
         )
 
         aggregator = LocationHourlyFeatureAggregator(
-            source_table="green_taxi_trips",
-            target_table="green_taxi_location_hourly_features",
-            postgres_uri="postgresql+psycopg2://admin:password@localhost:5432/mydb",
+            source_table=RAW_TABLE_NAME,
+            target_table=FEATURES_TABLE,
+            postgres_uri=POSTGRES_URI,
         )
         aggregator.run()
 
